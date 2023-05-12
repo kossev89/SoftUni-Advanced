24ab7cf220f8b649424627153348b7e67d0a4dad
kossev89 <kossev89@gmail.com>
2023-05-12 15:16:55 +0300

    Adding a solution to problem 1. Reverse a String

---
+++ Stacks and Queues - Lab/1. Reverse a String/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+string input = Console.ReadLine();
+Stack<char> chars= new Stack<char>();
+foreach (var item in input)
+{
+    chars.Push(item);
+}
+StringBuilder output = new StringBuilder();
+while (chars.Count>0)
+{
+    output.Append(chars.Pop());
+}
+Console.Write(output);
