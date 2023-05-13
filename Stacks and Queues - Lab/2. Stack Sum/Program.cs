83e4710ac4001dd98dd65851ead01c8bc4da1260
kossev89 <kossev89@gmail.com>
2023-05-13 22:46:56 +0300

    Adding a solution to problem 2. Stack Sum

---
+++ Stacks and Queues - Lab/2. Stack Sum/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] input = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+string commandInput = Console.ReadLine().ToLower();
+Stack<int> stack = new Stack<int>();
+foreach (var item in input)
+{
+    stack.Push(item);
+}
+
+while (commandInput != "end")
+{
+    string[] commandTokens = commandInput.Split(' ');
+    if (commandTokens[0] == "add")
+    {
+        stack.Push(int.Parse(commandTokens[1]));
+        stack.Push(int.Parse(commandTokens[2]));
+    }
+    else if (commandTokens[0] == "remove" && int.Parse(commandTokens[1]) <= stack.Count)
+    {
+        for (int i = 0; i < int.Parse(commandTokens[1]); i++)
+        {
+            stack.Pop();
+        }
+    }
+    commandInput = Console.ReadLine().ToLower();
+}
+Console.WriteLine($"Sum: {stack.Sum()}");
