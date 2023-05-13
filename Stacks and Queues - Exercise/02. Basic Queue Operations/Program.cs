aa8567caaac4c640681ec94abb17aa33388b3190
kossev89 <kossev89@gmail.com>
2023-05-14 01:17:00 +0300

    Adding a solution to problem 02. Basic Queue Oper

---
+++ Stacks and Queues - Exercise/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] inputInfo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int elementsToAdd = inputInfo[0];
+int elementsToRemove = inputInfo[1];
+int elementToFind = inputInfo[2];
+Queue<int> queue = new Queue<int>();
+
+int[] ints = Console.ReadLine().Split().Select(int.Parse).ToArray();
+for (int i = 0; i < elementsToAdd; i++)
+{
+    queue.Enqueue(ints[i]);
+}
+
+for (int i = 0; i < elementsToRemove; i++)
+{
+    queue.Dequeue();
+}
+
+if (queue.Contains(elementToFind))
+{
+    Console.WriteLine("true");
+}
+else if (queue.Count > 0)
+{
+    List<int> list = new List<int>();
+    while (queue.Count > 0)
+    {
+        list.Add(queue.Dequeue());
+    }
+    list.Sort();
+    Console.WriteLine(list[0]);
+}
+else
+{
+    Console.WriteLine("0");
+}
