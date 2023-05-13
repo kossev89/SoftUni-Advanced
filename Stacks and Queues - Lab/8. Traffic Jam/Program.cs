eb51ee65eff110bc9da941c3fc963ab2af09dbae
kossev89 <kossev89@gmail.com>
2023-05-14 00:41:16 +0300

    Adding a solution to problem 8. Traffic Jam

---
+++ Stacks and Queues - Lab/8. Traffic Jam/Program.cs
@@ -0,0 +1,26 @@
+﻿int numberOfCars = int.Parse(Console.ReadLine());
+string command = Console.ReadLine();
+Queue<string> cars = new Queue<string>();
+int totalCarsPassed = 0;
+
+while (command != "end")
+{
+    if (command == "green")
+    {
+        for (int i = 0; i < numberOfCars; i++)
+        {
+            if (cars.Count == 0)
+            {
+                break;
+            }
+            Console.WriteLine($"{cars.Dequeue()} passed!");
+            totalCarsPassed++;
+        }
+    }
+    else
+    {
+        cars.Enqueue(command);
+    }
+    command = Console.ReadLine();
+}
+Console.WriteLine($"{totalCarsPassed} cars passed the crossroads.");
