0b8ad9480977bccde5a454f122d6e69a7c6132c7
kossev89 <kossev89@gmail.com>
2023-05-13 23:40:00 +0300

    Adding a solution to problem 4. Matching Brackets

---
+++ Stacks and Queues - Lab/4. Matching Brackets/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+string input = Console.ReadLine();
+Stack<int> stack = new Stack<int>();
+StringBuilder StringBuilder = new StringBuilder();
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i] == '(')
+    {
+        stack.Push(i);
+    }
+    else if (input[i] == ')')
+    {
+        int startIndex = stack.Pop();
+        int endIndex = i;
+        int length = endIndex - startIndex +1;
+        StringBuilder.AppendLine(input.Substring(startIndex, length));
+    }
+}
+Console.WriteLine(StringBuilder);
