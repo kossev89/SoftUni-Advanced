e563bede1b152bcdcc42cbcd959e0f242a4fbfcb
kossev89 <kossev89@gmail.com>
2023-05-14 01:56:40 +0300

    Adding a solution to problem 04. Fast Food

---
+++ Stacks and Queues - Exercise/04. Fast Food/Program.cs
@@ -0,0 +1,24 @@
+﻿int totalFoodQuantity = int.Parse(Console.ReadLine());
+int[] ordersInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Queue<int> ordersQueue = new Queue<int>(ordersInput);
+Console.WriteLine(ordersQueue.Max());
+
+while (ordersQueue.Count > 0)
+{
+    int lastOrder = ordersQueue.Peek();
+    totalFoodQuantity -= ordersQueue.Dequeue();
+    if (totalFoodQuantity < 0)
+    {
+        ordersQueue.Enqueue(lastOrder);
+        break;
+    }
+}
+
+if (ordersQueue.Count > 0)
+{
+    Console.WriteLine($"Orders left: {string.Join(' ', ordersQueue)}");
+}
+else if (ordersQueue.Count == 0)
+{
+    Console.WriteLine("Orders complete");
+}
