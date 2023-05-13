7960964f98b08c3e51553d5320c5aa1596824c89
kossev89 <kossev89@gmail.com>
2023-05-14 00:24:22 +0300

    Adding a solution to problem 7. Hot Potato

---
+++ Stacks and Queues - Lab/7. Hot Potato/Program.cs
@@ -0,0 +1,14 @@
+﻿Queue<string> children = new Queue<string>(Console.ReadLine().Split());
+int numberOfThosses = int.Parse(Console.ReadLine());
+
+while (children.Count > 1)
+{
+    for (int i = 1; i < numberOfThosses; i++)
+    {
+        string name = children.Peek();
+        children.Dequeue();
+        children.Enqueue(name);
+    }
+    Console.WriteLine($"Removed {children.Dequeue()}");
+}
+Console.WriteLine($"Last is {children.Peek()}");
