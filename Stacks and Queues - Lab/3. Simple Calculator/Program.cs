55f4bc84e33a267d1f5c1e8fd997c540d2c9a6fc
kossev89 <kossev89@gmail.com>
2023-05-13 23:06:59 +0300

    Adding a solution to problem 3. Simple Calculator

---
+++ Stacks and Queues - Lab/3. Simple Calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿var input = Console.ReadLine().Split().ToArray();
+Array.Reverse(input);
+Stack<string> numbersAndOperations = new Stack<string>(input);
+int sum = int.Parse(numbersAndOperations.Pop());
+
+while (numbersAndOperations.Count > 0)
+{
+    string operation = numbersAndOperations.Pop();
+    int number = int.Parse(numbersAndOperations.Pop());
+    if (operation == "+")
+    {
+        sum += number;
+    }
+    else if (operation == "-")
+    {
+        sum -= number;
+    }
+}
+Console.WriteLine(sum);
