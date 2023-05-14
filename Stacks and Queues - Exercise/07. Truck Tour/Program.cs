4c8cf770f186f6e7639f32833bec67ab21544f41
kossev89 <kossev89@gmail.com>
2023-05-15 00:51:29 +0300

    Adding a solution to problem 07. Truck Tour

---
+++ Stacks and Queues - Exercise/07. Truck Tour/Program.cs
@@ -0,0 +1,44 @@
+﻿int numberOfStations = int.Parse(Console.ReadLine());
+Queue<int[]> stations = new();
+
+for (int i = 0; i < numberOfStations; i++)
+{
+    stations.Enqueue(Console.ReadLine().Split().Select(int.Parse).ToArray());
+}
+
+int startIndex = 0;
+
+while (true)
+{
+    bool isComplete = false;
+    int tank = 0;
+
+    foreach (var station in stations)
+    {
+        int fuelToAdd = station[0];
+        int distance = station[1];
+
+        tank += fuelToAdd;
+
+        if (tank - distance < 0)
+        {
+            isComplete = false;
+            startIndex++;
+            var currentStation = stations.Peek();
+            stations.Dequeue();
+            stations.Enqueue(currentStation);
+            break;
+        }
+        else
+        {
+            isComplete = true;
+            tank -= distance;
+        }
+    }
+
+    if (isComplete)
+    {
+        Console.WriteLine(startIndex);
+        break;
+    }
+}
