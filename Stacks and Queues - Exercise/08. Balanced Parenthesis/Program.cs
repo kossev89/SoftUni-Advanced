232c65ac8d79cc964663f06f588c97132751f4b4
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-05-15 23:01:52 +0300

    Adding a solution to 08. Balanced Parenthesis

---
+++ Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+char[] inputParenthesis = Console.ReadLine().ToArray();
+Stack<char> chars = new Stack<char>();
+
+foreach (var item in inputParenthesis)
+{
+    switch (item)
+    {
+        case '{':
+        case '[':
+        case '(':
+            chars.Push(item);
+            break;
+        case ')':
+            if (chars.Count == 0)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+            else if (chars.Pop() == '(')
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+        case ']':
+            if (chars.Count == 0)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+            else if (chars.Pop() == '[')
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+        case '}':
+            if (chars.Count == 0)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+            else if (chars.Pop() == '{')
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+    }
+}
+if (chars.Count == 0)
+{
+    Console.WriteLine("YES");
+}
