089ddb67e4e4ceb36f7da68e4ec16d8985c1f19b
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-05-16 22:55:40 +0300

    Adding a solution to problem 2. Sum Matrix Columns

---
+++ Multidimensional Arrays - Lab/2. Sum Matrix Columns/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] matrixSize = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+int rows = matrixSize[0];
+int cols = matrixSize[1];
+
+int[,] matrix = new int[rows, cols];
+for (int row = 0; row < rows; row++)
+{
+    int[] values = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    for (int col = 0; col < cols; col++)
+    {
+        matrix[row, col] = values[col];
+    }
+}
+for (int col = 0; col < matrix.GetLength(1); col++)
+{
+    int sum = 0;
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        sum += matrix[row, col];
+    }
+    Console.WriteLine(sum);
+}
