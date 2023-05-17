8bb9127d6dd0c466289582f6f10d777d1501ed2a
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-05-17 23:00:12 +0300

    Adding a solution to 2. Squares in Matrix

---
+++ Multidimensional Arrays - Exercise/2. Squares in Matrix/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] size = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+int rows = size[0];
+int cols = size[1];
+char[,] matrix = new char[rows, cols];
+
+for (int row = 0; row < rows; row++)
+{
+    char[] values = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(char.Parse)
+    .ToArray();
+    for (int col = 0; col < cols; col++)
+    {
+        matrix[row, col] = values[col];
+    }
+}
+
+int squares = 0;
+
+for (int row = 0; row < rows - 1; row++)
+{
+    for (int col = 0; col < cols - 1; col++)
+    {
+        if (matrix[row, col] == matrix[row, col + 1]
+            && matrix[row, col] == matrix[row + 1, col]
+            && matrix[row, col] == matrix[row + 1, col + 1]
+            )
+        {
+            squares++;
+        }
+    }
+}
+
+Console.WriteLine(squares);
