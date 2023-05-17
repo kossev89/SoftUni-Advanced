83400b811585633756c9985434daca59a44773a3
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-05-17 22:42:09 +0300

    Adding a solution to problem 1. Diagonal Difference

---
+++ Multidimensional Arrays - Exercise/1. Diagonal Difference/Program.cs
@@ -0,0 +1,26 @@
+﻿int size = int.Parse(Console.ReadLine());
+int[,] matrix = new int[size, size];
+
+for (int row = 0; row < size; row++)
+{
+    int[] values = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+    for (int col = 0; col < size; col++)
+    {
+        matrix[row, col] = values[col];
+    }
+}
+
+int sumPrime = 0;
+int sumSecond = 0;
+
+for (int i = 0; i < size; i++)
+{
+    sumPrime += matrix[i, i];
+    sumSecond += matrix[i, 2 - i];
+}
+
+int result = Math.Abs(sumPrime - sumSecond);
+Console.WriteLine(result);
---
+++ Stacks and Queues - Exercise/09. Simple Text Editor/Program.cs
@@ -0,0 +1,44 @@
+﻿string text = string.Empty;
+int numberOfCommands = int.Parse(Console.ReadLine());
+Stack<char> stack = new();
+Stack<char> undo = new();
+
+for (int i = 0; i < numberOfCommands; i++)
+{
+    string[] commandArg = Console.ReadLine().Split().ToArray();
+    int command = int.Parse(commandArg[0]);
+    switch (command)
+    {
+        case 1:
+            foreach (var item in stack.Reverse())
+            {
+                undo.Push(item);
+            }
+            foreach (var item in commandArg[1])
+            {
+                stack.Push(item);
+            }
+            break;
+        case 2:
+            foreach (var item in stack.Reverse())
+            {
+                undo.Push(item);
+            }
+            for (int j = 0; j < int.Parse(commandArg[1]); j++)
+            {
+                stack.Pop();
+            }
+            break;
+        case 3:
+            text = String.Join("", stack.Reverse());
+            Console.WriteLine(text[int.Parse(commandArg[1]) - 1]);
+            break;
+        case 4:
+            stack.Clear();
+            foreach (var item in undo.Reverse())
+            {
+                stack.Push(item);
+            }
+            break;
+    }
+}
