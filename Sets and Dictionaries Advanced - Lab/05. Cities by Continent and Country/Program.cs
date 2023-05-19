837e42f45cd330c1b2f060f6e12b13690f87c128
kossev89 <kossev89@gmail.com>
2023-05-20 00:29:04 +0300

    Adding a solution to problem 05. Cities by Contin

---
+++ Sets and Dictionaries Advanced - Lab/05. Cities by Continent and Country/Program.cs
@@ -0,0 +1,49 @@
+﻿var n = int.Parse(Console.ReadLine());
+var places = new Dictionary<string, Dictionary<string, List<string>>>();
+
+
+for (int i = 0; i < n; i++)
+{
+    var input = Console.ReadLine()
+    .Split();
+
+    var continent = input[0];
+    var country = input[1];
+    var city = input[2];
+    if (!places.ContainsKey(continent))
+    {
+        places[continent] = new Dictionary<string, List<string>>();
+        if (!places[continent].ContainsKey(country))
+        {
+            places[continent][country] = new List<string> { city };
+        }
+        else
+        {
+            places[continent][country].Add(city);
+        }
+    }
+    else
+    {
+        if (!places[continent].ContainsKey(country))
+        {
+            places[continent][country] = new List<string> { city };
+        }
+        else
+        {
+            places[continent][country].Add(city);
+        }
+    }
+}
+foreach (var place in places)
+{
+    Console.WriteLine($"{place.Key}:");
+    foreach (var item in place.Value)
+    {
+        Console.Write($" {item.Key} -> ");
+        Console.Write(string.Join(", ", item.Value));
+        Console.WriteLine();
+    }
+}
+
+
+
