b9b745a5cafc53760bd8432f5255c16e59052c99
kossev89 <kossev89@gmail.com>
2023-05-19 17:26:00 +0300

    Adding a solution to 01. Unique Usernames

---
+++ Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
@@ -0,0 +1,17 @@
+﻿var numberOfInputs = int.Parse(Console.ReadLine());
+var names = new List<string>();
+var name = string.Empty;
+
+for (int i = 0; i < numberOfInputs; i++)
+{
+    name = Console.ReadLine();
+	if (!names.Contains(name))
+	{
+		names.Add(name);
+	}
+}
+
+foreach (var item in names)
+{
+	Console.WriteLine(item);
+}
