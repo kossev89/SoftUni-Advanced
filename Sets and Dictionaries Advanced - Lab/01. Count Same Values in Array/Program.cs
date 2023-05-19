b67011f4ee34fd659137e07c30476703485f00ef
kossev89 <kossev89@gmail.com>
2023-05-19 19:23:21 +0300

    Adding a solution to problem 01.

---
+++ Sets and Dictionaries Advanced - Lab/01. Count Same Values in Array/Program.cs
@@ -0,0 +1,22 @@
+﻿var input = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(double.Parse);
+
+var numbers = new Dictionary<double, int>();
+
+foreach (var item in input)
+{
+    if (!numbers.ContainsKey(item))
+    {
+        numbers.Add(item, 1);
+    }
+    else
+    {
+        numbers[item]++;
+    }
+}
+
+foreach (var item in numbers)
+{
+    Console.WriteLine($"{item.Key} - {item.Value} times");
+}
