6f15ad7983b202dfdd75a9d040a09cb4ddd52b98
kossev89 <kossev89@gmail.com>
2023-05-19 18:46:15 +0300

    Adding a solution to problem 03.

---
+++ Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,14 @@
+﻿var numberOfinputs = int.Parse(Console.ReadLine());
+var elements = new HashSet<string>();
+
+for (int i = 0; i < numberOfinputs; i++)
+{
+    var input = Console.ReadLine()
+        .Split();
+    foreach (var item in input)
+    {
+        elements.Add(item);
+    }
+}
+var printSet = elements.OrderBy(x => x);
+Console.WriteLine(string.Join(' ', printSet));
