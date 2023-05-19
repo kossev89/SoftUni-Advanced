c84fa32b06f2c1db120a033b67beb3ec3cde2f99
kossev89 <kossev89@gmail.com>
2023-05-19 20:21:40 +0300

    Adding a solution to problem 02. and 03

---
+++ Sets and Dictionaries Advanced - Lab/02. Average Student Grades/Program.cs
@@ -0,0 +1,31 @@
+﻿var n = int.Parse(Console.ReadLine());
+var students = new Dictionary<string, List<decimal>>();
+
+for (int i = 0; i < n; i++)
+{
+    var input = Console.ReadLine()
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    var name = input[0];
+    var grade = decimal.Parse(input[1]);
+
+    if (students.ContainsKey(name))
+    {
+        students[name].Add(grade);
+    }
+    else
+    {
+        students.Add(name, new List<decimal>());
+        students[name].Add(grade);
+    }
+}
+
+foreach (var item in students)
+{
+    Console.Write($"{item.Key} -> ");
+    foreach (var grade in item.Value)
+    {
+        Console.Write($"{grade:f2} ");
+    }
+    Console.Write($"(avg: {item.Value.Average():f2})");
+    Console.WriteLine();
+}
---
+++ Sets and Dictionaries Advanced - Lab/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿var input = Console.ReadLine()
+    .Split()
+    .Select(int.Parse);
+
+var sorted = input.OrderByDescending(x => x).ToArray();
+
+if (sorted.Length > 3)
+{
+    Console.WriteLine(string.Join(' ', sorted.Take(3)));
+}
+else
+{
+    Console.WriteLine(string.Join(' ', sorted));
+}
