e72210f4443c92e5764301a00e177b04dcf13c5d
kossev89 <kossev89@gmail.com>
2023-05-20 11:37:05 +0300

    Adding a solution to 04. Even Times

---
+++ Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
@@ -0,0 +1,24 @@
+﻿var n = int.Parse(Console.ReadLine());
+
+var numbers = new Dictionary<int, int>();
+
+for (int i = 0; i < n; i++)
+{
+    var input = int.Parse(Console.ReadLine());
+    if (numbers.ContainsKey(input))
+    {
+        numbers[input]++;
+    }
+    else
+    {
+        numbers.Add(input, 1);
+    }
+}
+
+foreach (var item in numbers)
+{
+    if (item.Value % 2 == 0)
+    {
+        Console.WriteLine(item.Key);
+    }
+}
