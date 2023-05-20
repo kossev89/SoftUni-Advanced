fa70dc1b2a07c7cea7d2bf841231e6c21fc092e7
kossev89 <kossev89@gmail.com>
2023-05-20 11:44:04 +0300

    Adding a solution to 05. Count Symbols

---
+++ Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,20 @@
+﻿var chars = new SortedDictionary<char, int>();
+
+var input = Console.ReadLine().ToCharArray();
+
+foreach (var item in input)
+{
+    if (chars.ContainsKey(item))
+    {
+        chars[item]++;
+    }
+    else
+    {
+        chars.Add(item, 1);
+    }
+}
+
+foreach (var item in chars)
+{
+    Console.WriteLine($"{item.Key}: {item.Value} time/s");
+}
