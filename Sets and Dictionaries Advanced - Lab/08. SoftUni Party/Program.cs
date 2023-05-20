11f1c48994a680932c76ee89e348ad7936990d10
kossev89 <kossev89@gmail.com>
2023-05-20 11:27:53 +0300

    Adding a solution to 08. SoftUni Party

---
+++ Sets and Dictionaries Advanced - Lab/08. SoftUni Party/Program.cs
@@ -0,0 +1,46 @@
+﻿var vipGuests = new HashSet<string>();
+var regularGuests = new HashSet<string>();
+
+var input = Console.ReadLine();
+
+while (input != "PARTY")
+{
+    var first = input[0];
+    if (char.IsDigit(first))
+    {
+        vipGuests.Add(input);
+    }
+    else
+    {
+        regularGuests.Add(input);
+    }
+    input = Console.ReadLine();
+}
+input = Console.ReadLine();
+
+while (input != "END")
+{
+    var first = input[0];
+    if (char.IsDigit(first))
+    {
+        vipGuests.Remove(input);
+    }
+    else
+    {
+        regularGuests.Remove(input);
+    }
+    input = Console.ReadLine();
+}
+
+Console.WriteLine(vipGuests.Count + regularGuests.Count);
+
+foreach (var item in vipGuests)
+{
+    Console.WriteLine(item);
+}
+
+foreach (var item in regularGuests)
+{
+    Console.WriteLine(item);
+}
+
