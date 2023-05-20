3d756d8080fd261b0ce1055ca96859995f9cd199
kossev89 <kossev89@gmail.com>
2023-05-20 16:20:10 +0300

    Adding a solution to 10. Crossroads

---
+++ Stacks and Queues - Exercise/10. Crossroads/Program.cs
@@ -0,0 +1,52 @@
+﻿var greenLight = int.Parse(Console.ReadLine());
+var freeWindow = int.Parse(Console.ReadLine());
+var carsQueue = new Queue<string>();
+
+var command = Console.ReadLine();
+var carsPassed = 0;
+var isCrashed = false;
+
+while (command != "END")
+{
+    switch (command)
+    {
+        case "green":
+            var timeGreen = greenLight;
+            var timeFree = freeWindow;
+
+            while (timeGreen > 0 && carsQueue.Count > 0)
+            {
+                var currentCar = carsQueue.Peek();
+                if ((timeGreen + timeFree) >= currentCar.Length)
+                {
+                    timeGreen -= currentCar.Length;
+                    carsQueue.Dequeue();
+                    carsPassed++;
+                }
+                else
+                {
+                    Console.WriteLine("A crash happened!");
+                    Console.WriteLine($"{currentCar} was hit at {currentCar[timeGreen + timeFree]}");
+                    isCrashed = true;
+                    break;
+                }
+            }
+            break;
+        default:
+            carsQueue.Enqueue(command);
+            break;
+    }
+    if (isCrashed == true)
+    {
+        break;
+    }
+    else
+    {
+        command = Console.ReadLine();
+    }
+}
+if (isCrashed == false)
+{
+    Console.WriteLine("Everyone is safe.");
+    Console.WriteLine($"{carsPassed} total cars passed the crossroads.");
+}
