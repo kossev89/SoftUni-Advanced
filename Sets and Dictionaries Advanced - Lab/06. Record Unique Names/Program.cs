d6e49e45445eb920d760772591206806903c9332
kossev89 <kossev89@gmail.com>
2023-05-20 11:11:01 +0300

    Adding a solution to problem 07. Parking Lot

---
+++ Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
@@ -0,0 +1,13 @@
+﻿var n = int.Parse(Console.ReadLine());
+var names = new HashSet<string>();
+
+for (int i = 0; i < n; i++)
+{
+    var name = Console.ReadLine();
+    names.Add(name);
+}
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
---
+++ Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,33 @@
+﻿var command = Console.ReadLine();
+var parkLot = new HashSet<string>();
+
+while (command != "END")
+{
+    var tokens = command
+        .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+    var direction = tokens[0];
+    var carNumber = tokens[1];
+
+    switch (direction)
+    {
+        case "IN":
+            parkLot.Add(carNumber); break;
+        case "OUT":
+            parkLot.Remove(carNumber); break;
+
+        default:
+            break;
+    }
+    command = Console.ReadLine();
+}
+if (parkLot.Count > 0)
+{
+    foreach (var car in parkLot)
+    {
+        Console.WriteLine(car);
+    }
+}
+else
+{
+    Console.WriteLine("Parking Lot is Empty");
+}
