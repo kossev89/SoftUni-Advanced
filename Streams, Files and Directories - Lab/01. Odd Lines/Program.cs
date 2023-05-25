990d8258859bb8f98346d6a2af66e0b0b37d1d8f
kossev89 <kossev89@gmail.com>
2023-05-25 17:02:24 +0300

    Adding a solution to 01. Odd Lines

---
+++ Streams, Files and Directories - Lab/01. Odd Lines/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace OddLines
+{
+    using System.IO;
+
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            var reader = new StreamReader(inputFilePath);
+            var lines = reader.ReadToEnd();
+            var writer = new StreamWriter(outputFilePath);
+            var counter =0;
+            using (reader)
+            {
+                while (lines != null)
+                {
+
+                }
+            }
+
+        }
+    }
+}
