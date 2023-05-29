abbc1e40397d6720c4048e1cf283cdf32170e905
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-05-29 23:57:35 +0300

    Adding solutions to problems 04 and 05

---
+++ Functional Programming - Exercise/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+var range = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+List<int> numbers = new();
+
+Predicate<int> isEven = number =>
+{
+    if (number % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+};
+
+for (int i = range[0]; i <= range.Last(); i++)
+{
+    numbers.Add(i);
+}
+
+string command = Console.ReadLine();
+
+foreach (var item in numbers)
+{
+    switch (command)
+    {
+        case "even":
+            if (isEven(item))
+            {
+                Console.Write($"{item} ");
+            }
+            break;
+        case "odd":
+            if (isEven(item) == false)
+            {
+                Console.Write($"{item} ");
+            }
+            break;
+        default:
+            break;
+    }
+}
---
+++ Functional Programming - Exercise/05. Applied Arithmetics/Program.cs
@@ -0,0 +1,41 @@
+﻿Func<string, int[], int[]> calculate = (operation, inputNumbers) =>
+{
+    switch (operation)
+    {
+        case "add":
+            for (int i = 0; i < inputNumbers.Length; i++)
+            {
+                inputNumbers[i]++;
+            }
+            break;
+        case "multiply":
+            for (int i = 0; i < inputNumbers.Length; i++)
+            {
+                inputNumbers[i] *= 2;
+            }
+            break;
+        case "subtract":
+            for (int i = 0; i < inputNumbers.Length; i++)
+            {
+                inputNumbers[i]--;
+            }
+            break;
+        case "print":
+            Console.WriteLine(String.Join(' ', inputNumbers));
+            break;
+    }
+    return inputNumbers;
+};
+
+int[] inputNumbers = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+string operation = Console.ReadLine();
+
+while (operation != "end")
+{
+    calculate(operation, inputNumbers);
+    operation = Console.ReadLine();
+}
