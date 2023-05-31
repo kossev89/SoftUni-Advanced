ca977ab70984a2c76c9f9d0761dd7f323b118d25
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-06-01 01:12:38 +0300

    Adding solutions to problems 09 and 10

---
+++ Functional Programming - Exercise/09. Predicate Party!/Program.cs
@@ -0,0 +1,116 @@
+﻿using System.Text;
+
+List<string> people = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+string[] commandTokens = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+StringBuilder stringBuilder = new();
+
+
+Func<string, string, string, List<string>> goingToParty = (action, condition, statement) =>
+{
+    List<string> result = new List<string>(people);
+    switch (action)
+    {
+        case "Double":
+            switch (condition)
+            {
+                case "StartsWith":
+                    foreach (var item in people)
+                    {
+                        if (item.StartsWith(statement))
+                        {
+                            result.Insert(people.IndexOf(item), item);
+                        }
+                    }
+                    break;
+                case "Length":
+                    foreach (var item in people)
+                    {
+                        if (item.Length == int.Parse(statement))
+                        {
+                            result.Insert(people.IndexOf(item), item);
+                        }
+                    }
+                    break;
+                case "EndsWith":
+                    foreach (var item in people)
+                    {
+                        if (item.EndsWith(statement))
+                        {
+                            result.Insert(people.IndexOf(item), item);
+                        }
+                    }
+                    break;
+            }
+            break;
+        case "Remove":
+            switch (condition)
+            {
+                case "StartsWith":
+                    foreach (var item in people)
+                    {
+                        if (item.StartsWith(statement))
+                        {
+                            result.Remove(item);
+                        }
+                    }
+                    break;
+                case "Length":
+                    foreach (var item in people)
+                    {
+                        if (item.Length == int.Parse(statement))
+                        {
+                            result.Remove(item);
+                        }
+                    }
+                    break;
+                case "EndsWith":
+                    foreach (var item in people)
+                    {
+                        if (item.EndsWith(statement))
+                        {
+                            result.Remove(item);
+                        }
+                    }
+                    break;
+            }
+            break;
+    }
+    people = result;
+    return people;
+};
+
+while (commandTokens[0] != "Party!")
+{
+    string action = commandTokens[0];
+    string condition = commandTokens[1];
+    string statement = commandTokens[2];
+    people = goingToParty(action, condition, statement);
+    commandTokens = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+}
+
+if (people.Count == 0)
+{
+    Console.WriteLine("Nobody is going to the party!");
+    return;
+}
+else
+{
+    for (int i = 0; i < people.Count; i++)
+    {
+        if (i != people.Count-1)
+        {
+            stringBuilder.Append($"{people[i]}, ");
+        }
+        else
+        {
+            stringBuilder.Append($"{people[i]} ");
+        }
+    }
+}
+stringBuilder.Append("are going to the party!");
+Console.WriteLine(stringBuilder);
---
+++ Functional Programming - Exercise/10. The Party Reservation Filter Module/Program.cs
@@ -0,0 +1,52 @@
+﻿List<string> invitations = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+List<string> filtered = new(invitations);
+string[] commandTokens = Console.ReadLine()
+    .Split(';', StringSplitOptions.RemoveEmptyEntries)
+    .ToArray();
+
+while (commandTokens[0] != "Print")
+{
+    string command = commandTokens[0];
+    string filterType = commandTokens[1];
+    string filterValue = commandTokens[2];
+    if (command=="Add filter")
+    {
+        filtered.RemoveAll(Filter(filterType, filterValue));
+    }
+    else if (command == "Remove filter")
+    {
+        List<string> peopleToAdd = invitations.FindAll(Filter(filterType, filterValue));
+        foreach (var item in peopleToAdd)
+        {
+            filtered.Add(item);
+        }
+    }
+    commandTokens = Console.ReadLine()
+    .Split(';', StringSplitOptions.RemoveEmptyEntries)
+    .ToArray();
+}
+Console.WriteLine(String.Join(' ', filtered));
+
+static Predicate<string> Filter(string filterType, string filterValue)
+{
+    switch (filterType)
+    {
+        case "Ends with":
+            return x => x.EndsWith(filterValue);
+            break;
+        case "Starts with":
+            return x => x.StartsWith(filterValue);
+            break;
+
+        case "Length":
+            return x => x.Length == int.Parse(filterValue);
+            break;
+        case "Contains":
+            return x => x.Contains(filterValue);
+            break;
+        default:
+            return default;
+    }
+}
