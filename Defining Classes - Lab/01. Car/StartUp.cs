0319be33aeabb6e6c4c0616d9b2e99d44816f63d
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-06-01 22:53:30 +0300

    Adding a solution Defining Classes - Lab
    Adding solution to problem 01. Car

---
+++ Defining Classes - Lab/01. Car/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new();
+            car.Make = "BMW";
+            car.Model = "e35";
+            car.Year = 2008;
+        }
+    }
+}
+
