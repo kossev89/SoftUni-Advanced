9ff2487425e21857f2c40757b6a858bf54f85803
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-06-01 23:37:50 +0300

    Adding a new solution
    Adding a solution to problem 02. Car Extension

---
+++ Defining Classes - Lab/02. Car Extension/Car.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumtion;
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+        public double FuelConsumption
+        {
+            get { return fuelConsumtion; }
+            set { fuelConsumtion = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            if (this.FuelQuantity - (this.FuelConsumption * distance) >= 0)
+            {
+                this.FuelQuantity -= (this.FuelConsumption * distance);
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            StringBuilder sb = new();
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"Year: {this.Year}");
+            sb.AppendLine($"Fuel: {this.FuelQuantity:f2}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ Defining Classes - Lab/02. Car Extension/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new();
+            car.Make = "VW";
+            car.Model = "Golf MK4";
+            car.Year = 1998;
+            car.FuelConsumption = 200;
+            car.FuelQuantity = 200;
+
+            car.Drive(0.5);
+            Console.WriteLine(car.WhoAmI());
+            car.Drive(0.5);
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
