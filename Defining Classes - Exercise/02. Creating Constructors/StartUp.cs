ff166f501a527cb2e02510c3b98ae8ad9fe75909
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-06-03 00:05:33 +0300

    Adding a solution to 02. Creating Constructors

---
+++ Defining Classes - Exercise/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person first = new();
+            first.Name = "Peter";
+            first.Age = 20;
+            Person second = new();
+            second.Name = "George";
+            second.Age = 18;
+            Person third = new();
+            third.Name = "Jose";
+            third.Age = 43;
+
+            Console.WriteLine(third.Age);
+        }
+    }
+}
