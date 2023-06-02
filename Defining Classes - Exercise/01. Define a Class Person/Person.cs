013847ddba46402498119bd22dd312d16641750e
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-06-02 23:59:23 +0300

    Adding a solution to problem 01. Define a Class Person

---
+++ Defining Classes - Exercise/01. Define a Class Person/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+		private string name;
+		private int age;
+
+		public Person()
+		{
+
+		}
+
+		public int Age
+		{
+			get { return age; }
+			set { age = value; }
+		}
+
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+	}
+}
