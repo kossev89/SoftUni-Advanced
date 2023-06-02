bc1092c0007af7506ef743fcc6474992e2af9c2f
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-06-03 02:28:13 +0300

    Adding solution to a problem

---
+++ Defining Classes - Exercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.DateModifier
+{
+    public class DateModifier
+    {
+        private string firstDate;
+        private string secondDate;
+
+        public DateModifier(string firstDate, string secondDate)
+        {
+            FirstDate = firstDate;
+            SecondDate = secondDate;
+            SecondDate = secondDate;
+            FirstDate = firstDate;
+        }
+
+        public string SecondDate
+        {
+            get { return secondDate; }
+            set { secondDate = value; }
+        }
+
+
+        public string FirstDate
+        {
+            get { return firstDate; }
+            set { firstDate = value; }
+        }
+
+        public double CalculateDate(DateModifier dateModifier)
+        {
+            DateTime first = DateTime.Parse(dateModifier.FirstDate);
+            DateTime second = DateTime.Parse(dateModifier.SecondDate);
+            double totalDays = (second - first).TotalDays;
+            return totalDays;
+        }
+
+    }
+}
---
+++ Defining Classes - Exercise/05.DateModifier/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace _05.DateModifier
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+            DateModifier dateModifier = new DateModifier(first, second);
+            double difference = dateModifier.CalculateDate(dateModifier);
+            Console.WriteLine(Math.Abs(difference));
+        }
+    }
+}
