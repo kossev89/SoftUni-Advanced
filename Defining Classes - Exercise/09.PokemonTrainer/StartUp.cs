cc9f97ad3277f6d0d467dde20bf1cc2f159203f8
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-06-05 19:44:36 +0300

    Adding a solution to problem 09

---
+++ Defining Classes - Exercise/09.PokemonTrainer/StartUp.cs
@@ -0,0 +1,52 @@
+﻿namespace _09.PokemonTrainer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Trainer> trainers = new();
+            Dictionary<string, int> trainerNames = new();
+            string[] inputLine = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            while (inputLine[0] != "Tournament")
+            {
+                string trainerName = inputLine[0];
+                string pokemonName = inputLine[1];
+                string pokemonElement = inputLine[2];
+                int pokemonHealth = int.Parse(inputLine[3]);
+                Pokemon pokemon = new(pokemonName, pokemonElement, pokemonHealth);
+                if (trainerNames.ContainsKey(trainerName))
+                {
+                    Trainer currentTrainer = trainers.FirstOrDefault(x => x.Name == trainerName);
+                    currentTrainer.pokemons.Add(pokemon);
+                }
+                else
+                {
+                    Trainer trainer = new(trainerName);
+                    trainer.pokemons.Add(pokemon);
+                    trainers.Add(trainer);
+                    trainerNames.Add(trainerName,0);
+                }
+                inputLine = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            string commandElement = Console.ReadLine();
+            while (commandElement != "End")
+            {
+                foreach (var item in trainers)
+                {
+                    item.Fight(item, commandElement);
+                }
+                commandElement = Console.ReadLine();
+            }
+            var trainersSorted =trainers.OrderByDescending(x => x.Badges).ToList();
+
+            foreach (var item in trainersSorted)
+            {
+                Console.WriteLine($"{item.Name} {item.Badges} {item.pokemons.Count}");
+            }
+        }
+    }
+}
---
+++ Defining Classes - Exercise/09.PokemonTrainer/Trainer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.PokemonTrainer
+{
+    public class Trainer
+    {
+        public Trainer(string name)
+        {
+            Name = name;
+            Badges = 0;
+            pokemons = new();
+        }
+
+        public string Name { get; set; }
+        public int Badges { get; set; }
+        public List<Pokemon> pokemons { get; set; }
+        public void Fight(Trainer trainer, string element)
+        {
+            bool win = false;
+            foreach (var item in trainer.pokemons)
+            {
+                if (item.Element == element)
+                {
+                    win = true;
+                    trainer.Badges++;
+                    return;
+                }
+            }
+            if (win == false)
+            {
+                List<Pokemon> deadPokemons = new();
+                foreach (var item in trainer.pokemons)
+                {
+                    item.Health -= 10;
+                    if (item.Health <= 0)
+                    {
+                        deadPokemons.Add(item);
+                    }
+                }
+                if (deadPokemons.Count > 0)
+                {
+                    foreach (var item in deadPokemons)
+                    {
+                        trainer.pokemons.Remove(item);
+                    }
+                }
+                return;
+            }
+        }
+    } 
+}
