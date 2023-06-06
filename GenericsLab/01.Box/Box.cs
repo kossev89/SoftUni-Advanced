e546d4c0e42d5bbfc7e121616ea2462c87b79f14
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-06-07 00:26:12 +0300

    Adding a solution to problem 01.Box

---
+++ GenericsLab/01.Box/Box.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public Box()
+        {
+            List = new();
+        }
+
+        public List<T> List { get; set; }
+
+        public void Add(T item)
+        {
+            List.Add(item);
+        }
+        public T Remove()
+        {
+            T lastElement = List.Last();
+            List.RemoveAt(List.Count - 1);
+            return lastElement;
+        }
+
+        public int Count
+        {
+            get { return List.Count; }
+        }
+    }
+}
---
+++ GenericsLab/01.Box/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace BoxOfT
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+
+            Console.WriteLine(box.Remove());
+        }
+    }
+}
