97096c4139bbac7c124b2967662f3a497e5e16f9
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-06-07 23:06:19 +0300

    Adding a solution to problem GenericSwapMethodString

---
+++ GenericsExercise/GenericSwapMethodString/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace GenericSwapMethodString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new();
+            int itemCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < itemCount; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+            int[] command = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Swap(list, command[0], command[1]);
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item.GetType()}: {item}");
+            }
+        }
+
+        static void Swap<T>(List<T> list, int firsIndex, int secondIndex)
+        {
+            T temp = list[firsIndex];
+            list[firsIndex] = list[secondIndex];
+            list[secondIndex] = temp;
+        }
+    }
+}
