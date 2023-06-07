d723a4c45bc573346056fc227162af24e2b65c56
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-06-07 22:47:16 +0300

    Adding a solution to problem GenericBoxofInteger

---
+++ GenericsExercise/GenericBoxofInteger/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxofInteger
+{
+    public class Box<T>
+    {
+        public Box(T intValue)
+        {
+            this.value = intValue;
+        }
+
+        public T value { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder output = new();
+            output.Append($"{value.GetType()}: {value}");
+            return output.ToString().Trim();
+        }
+    }
+}
---
+++ GenericsExercise/GenericBoxofInteger/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace GenericBoxofInteger
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int boxesCount = int.Parse(Console.ReadLine());
+            List<Box<int>> boxes = new();
+
+            for (int i = 0; i < boxesCount; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                Box<int> box = new Box<int>(input);
+                boxes.Add(box);
+            }
+
+            foreach (var item in boxes)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
