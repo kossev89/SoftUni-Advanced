9d27bdebf4a8fbdbedb7611d6594c0ef622ab2ff
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-06-08 00:26:33 +0300

    Adding solution to Generic Count Method String

---
+++ GenericsExercise/GenericCountMethodString/Box.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCountMethodString
+{
+    public class Box<T> where T : IComparable<T>
+    {
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder output = new();
+            output.Append($"{Value.GetType()}: {Value}");
+            return output.ToString().Trim();
+        }
+
+        public bool CompareToElement (Box<T> box, T element)
+        {
+            bool isBigger = false;
+            if (box.Value.CompareTo(element)>0)
+            {
+                isBigger = true;
+            }
+            return isBigger;
+        }
+
+    }
+}
---
+++ GenericsExercise/GenericCountMethodString/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace GenericCountMethodString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int boxesCount = int.Parse(Console.ReadLine());
+            List<Box<string>> boxes = new();
+
+            for (int i = 0; i < boxesCount; i++)
+            {
+                string input = Console.ReadLine();
+                Box<string> box = new Box<string>(input);
+                boxes.Add(box);
+            }
+
+            string elementToComare = Console.ReadLine();
+            int counter = 0;
+            foreach (var item in boxes)
+            {
+                if (item.CompareToElement(item,elementToComare))
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter.ToString());
+        }
+    }
+}
