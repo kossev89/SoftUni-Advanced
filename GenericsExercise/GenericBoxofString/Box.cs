97f54b9e911ed5fec4e05d86874ed46c21e85caf
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-06-07 22:40:02 +0300

    Adding a solution to problem GenericBoxofString

---
+++ GenericsExercise/GenericBoxofString/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxofString
+{
+    public class Box<T>
+    {
+        public Box(T stringValue)
+        {
+            this.stringValue = stringValue;
+        }
+
+        public T stringValue { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder output = new();
+            output.Append($"{stringValue.GetType()}: {stringValue}");
+            return output.ToString().Trim();
+        }
+    }
+}
---
+++ GenericsExercise/GenericBoxofString/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace GenericBoxofString
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int boxesCount = int.Parse(Console.ReadLine());
+            List < Box<string> > boxes= new();
+
+            for (int i = 0; i < boxesCount; i++)
+            {
+                string input = Console.ReadLine();
+                Box<string> box = new Box<string>(input);
+                boxes.Add(box);
+            }
+
+            foreach (var item in boxes)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
