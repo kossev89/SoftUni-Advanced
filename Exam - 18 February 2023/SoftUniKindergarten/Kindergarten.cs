1fdc0a28c4f50e69b2b2e37318cb9a84be9698a2
kossev89 <kossev89@gmail.com>
2023-06-15 08:31:12 +0300

    Adding a solution to problem Kindergarden

---
+++ Exam - 18 February 2023/SoftUniKindergarten/Kindergarten.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace SoftUniKindergarten
+{
+    public class Kindergarten
+    {
+        public Kindergarten(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            Registry = new();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public List<Child> Registry { get; set; }
+
+        public bool AddChild(Child child)
+        {
+            bool childAdded = false;
+            if (Capacity > Registry.Count)
+            {
+                Registry.Add(child);
+                childAdded = true;
+            }
+            return childAdded;
+        }
+
+        public bool RemoveChild(string childFullName)
+        {
+            bool childRemoved = false;
+            string[] names = childFullName
+                .Split(' ', System.StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in Registry)
+            {
+                if (item.FirstName == names[0] && item.LastName == names[1])
+                {
+                    childRemoved = true;
+                    Registry.Remove(item);
+                    break;
+                }
+            }
+            return childRemoved;
+        }
+
+        public int ChildrenCount()
+        {
+            int count = Registry.Count;
+            return count;
+        }
+
+        public string GetChild(string childFullName)
+        {
+            Child result = default;
+            string[] names = childFullName
+                .Split(' ', System.StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in Registry)
+            {
+                if (item.FirstName == names[0] && item.LastName == names[1])
+                {
+                    result = item;
+                    break;
+                }
+
+            }
+            if (result == default)
+            {
+                return null;
+            }
+            else
+            {
+                return result.ToString();
+            }
+        }
+
+        public string RegistryReport()
+        {
+            StringBuilder report = new();
+            var registrySorted = Registry.OrderByDescending(x => x.Age)
+                .ThenBy(x => x.LastName)
+                .ThenBy(x => x.FirstName)
+                .ToList();
+            report.AppendLine($"Registered children in {Name}:");
+            foreach (var item in registrySorted)
+            {
+                report.AppendLine(item.ToString());
+            }
+            return report.ToString().Trim();
+        }
+
+
+    }
+}
---
+++ Exam - 18 February 2023/SoftUniKindergarten/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SoftUniKindergarten
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Kindergarten kindergarten = new Kindergarten("Space S", 3);
+            Child childOne = new Child("Ruzha", "Garbo", 6, "Karl Gustafsson", "0468 888 888");
+            Child childTwo = new Child("Ruzha", "Muskova", 6, "Maye Musk", " 1 888 518 3752");
+            Child childThree = new Child("George", "Bush", 5, " George Bush Sr.", "xx xxx xxx xxx");
+            Child childFour = new Child("Ruzha", "Ignatova", 6, "Veska Ignatova", "+49 30 901820");
+            Child childFive = new Child("Greta", "Thinberg", 3, "Allen White", "541-754-3010");
+            Child childSix = new Child("T", "Rex", 2, "Steven Spielberg", "63 001 09 93");
+            Child childSeven = new Child("S", "Rex", 2, " Steven Spielberg ", "63 001 09 93");
+            Child childEight = new Child("Greta", "Thunberg", 3, "Pablo Gaviria", "0888 888 888");
+            Child childNine = new Child("Tim", "Duncan", 6, "William Duncan", "(555) 555-1234");
+            Console.WriteLine(kindergarten.AddChild(childOne));
+            Console.WriteLine(kindergarten.AddChild(childTwo));
+            Console.WriteLine(kindergarten.AddChild(childThree));
+            Console.WriteLine(kindergarten.AddChild(childFour));
+            Console.WriteLine(kindergarten.RemoveChild("George Bush"));
+            Console.WriteLine(kindergarten.AddChild(childFour));
+            Console.WriteLine(kindergarten.RegistryReport());
+            Console.WriteLine(kindergarten.GetChild("Ruzha Ignatova"));
+            Console.WriteLine(kindergarten.GetChild("Ruzha Igdsad"));
+        }
+    }
+}
